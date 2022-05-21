7bc49a438cde27757b16436ebf51657ec97a7b6c
fyagiz <furkanyagiz@gmail.com>
2022-05-21 18:35:46 +0300

    Add scroll for goals

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput, ScrollView } from "react-native";
 
 export default function App() {
     const [enteredGoalText, setEnteredGoalText] = useState<string>("");
@@ -24,11 +24,13 @@ export default function App() {
                 <Button title="Add Goal" onPress={addGoalHandler} />
             </View>
             <View style={styles.goalsContainer}>
-                {goals.map((goal) => (
-                    <View style={styles.goalItem} key={goal}>
-                        <Text style={styles.goalText}>{goal}</Text>
-                    </View>
-                ))}
+                <ScrollView >
+                    {goals.map((goal) => (
+                        <View style={styles.goalItem} key={goal}>
+                            <Text style={styles.goalText}>{goal}</Text>
+                        </View>
+                    ))}
+                </ScrollView>
             </View>
         </View>
     );
@@ -67,5 +69,5 @@ const styles = StyleSheet.create({
     },
     goalText: {
         color: "white",
-    }
+    },
 });
