2541ce9d9d80b9a0f84712bef1e99595b595019b
fyagiz <furkanyagiz@gmail.com>
2022-06-13 00:16:23 +0300

    Improvements game logic

--- src/screens/GameScreen.tsx
+++ src/screens/GameScreen.tsx
@@ -1,23 +1,45 @@
-import { Text, View, SafeAreaView } from "react-native";
-import { useState } from "react";
+import { Text, View, Alert } from "react-native";
+import { ReactNode, useState } from "react";
 import Title from "../components/ui/Title";
 import { gameScreenStyles } from "../styles/screenStyles";
 import { GameScreenProps } from "../types/PropTypes";
 import NumberContainer from "../components/game/NumberContainer";
+import PrimaryButton from "../components/ui/PrimaryButton";
 
-function generateRandomBetween(min: number, max: number, exclude: number): number {
+function generateRandomNumber(min: number, max: number, exclude: number): number {
     const randomNumber = Math.floor(Math.random() * (max - min)) + min;
 
     if (randomNumber === exclude) {
-        return generateRandomBetween(min, max, exclude);
+        return generateRandomNumber(min, max, exclude);
     } else {
         return randomNumber;
     }
 }
 
-function GameScreen(props: GameScreenProps) {
-    const initialGuess = generateRandomBetween(1, 100, props.userNumber);
+let minBoundary = 1;
+let maxBoundary = 100;
+
+function GameScreen(this: ReactNode, props: GameScreenProps) {
+    const initialGuess = generateRandomNumber(minBoundary, maxBoundary, props.userNumber);
     const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+    function nextGuessHandler(isBigger: boolean) {
+        if (
+            (isBigger === false && currentGuess < props.userNumber) ||
+            (isBigger === true && currentGuess > props.userNumber)
+        ) {
+            Alert.alert("Dont lie!", "You know that this is wrong!", [{text: "Sorry!", style:"cancel"}])
+            return;
+        }
+            if (isBigger === false) {
+                maxBoundary = currentGuess;
+            } else {
+                minBoundary = currentGuess + 1;
+            }
+        const newRandomNumber = generateRandomNumber(minBoundary, maxBoundary, currentGuess);
+        setCurrentGuess(newRandomNumber);
+    }
+
     return (
         <View style={gameScreenStyles.screen}>
             <View>
@@ -26,7 +48,10 @@ function GameScreen(props: GameScreenProps) {
             </View>
             <View>
                 <Text>Higher or Lower?</Text>
-                {/* + - */}
+                <View>
+                    <PrimaryButton onPress={nextGuessHandler.bind(this, false)}>-</PrimaryButton>
+                    <PrimaryButton onPress={nextGuessHandler.bind(this, true)}>+</PrimaryButton>
+                </View>
             </View>
             <View>{/* LOG */}</View>
         </View>
--- src/types/PropTypes.ts
+++ src/types/PropTypes.ts
@@ -1,9 +1,8 @@
-type voidType = () => void;
 type numberToVoid = (pickedNumber: number) => void;
 
 export type PrimaryButtonProps = {
     children?: string;
-    onPress?: voidType;
+    onPress?: any;
 };
 
 export type StartGameScreenProps = {
